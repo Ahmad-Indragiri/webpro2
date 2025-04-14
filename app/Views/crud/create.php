998654428fd07d9d661b4fbb6323f2cdc3789ad8
Ahmad-Indragiri <ahmadindgr225@gmail.com>
2025-04-14 13:38:47 +0700

    Initial commit: Update Codeigniter4

--- app/Views/crud/create.php
+++ app/Views/crud/create.php
@@ -21,12 +21,12 @@
                         <input type="text" class="form-control" id="nama" name="nama" placeholder="Masukkan Nama" required>
                     </div>
                     <div class="mb-3">
-                        <label for="email" class="form-label">Email</label>
-                        <input type="email" class="form-control" id="email" name="email" placeholder="Masukkan Email" required>
+                        <label for="alamat" class="form-label">Alamat</label>
+                        <input type="alamat" class="form-control" id="alamat" name="alamat" placeholder="Masukkan Alamat" required>
                     </div>
                     <div class="mb-3">
-                        <label for="alamat" class="form-label">Alamat</label>
-                        <textarea class="form-control" id="alamat" name="alamat" rows="3" placeholder="Masukkan Alamat" required></textarea>
+                        <label for="pekerjaan" class="form-label">Pekerjaan</label>
+                        <textarea class="form-control" id="pekerjaan" name="pekerjaan" rows="3" placeholder="Masukkan Pekerjaan" required></textarea>
                     </div>
                     <button type="submit" class="btn btn-primary">Simpan</button>
                     <a href="<?= site_url('/') ?>" class="btn btn-secondary">Kembali</a>
